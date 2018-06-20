9f12e6ba9e37ec99f6ecc9fb5fb8735e28b56a00
Cédric David <cedric.david@btopenworld.com>
2018-06-21 01:00:11 +0200

    Create index.js

---
+++ index.js
@@ -0,0 +1,10 @@
+function titleCase(str) {
+  let arr = str.split(" ");
+ for (let i = 0; i < arr.length; i++){
+   arr[i] = arr[i].charAt(0).toUpperCase() + arr[i].slice(1).toLowerCase();
+ }
+   return arr.join(" ");  
+  
+}
+
+titleCase("I'm a little tea pot");
